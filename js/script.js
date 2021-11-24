a85871c3318af55d2feb14d25967dab1a1fe5c6c
Andrey Ovcharenko <plowand@gmail.com>
2021-11-24 18:26:20 +0200

    xxx

--- js/script.js
+++ js/script.js
@@ -67,3 +67,42 @@ function EditRecord() {
     adoRS.Close();
     adoConn.Close();
 }
+
+
+// https://www.froglogic.com/blog/tip-of-the-week/sqlite-driven-testing-using-javascript/
+function fetchDataFromSQLiteDB() {
+    //used http://sqlitebrowser.org/ for creating database
+    //var conn = SQL.connect( { Driver: "SQLite", Database: "C:\\Users\\franke\\Desktop\\PCMDBase.db"} );
+    var conn = SQL.connect({ Driver: "SQLite", Database: "\\PCMDBase.db" });
+
+    var result = conn.query("SELECT id, forename, surname, email, phone FROM addressbook;");
+    if (result.isValid == false) {
+        test.log("Result is not valid, maybe no entries in database?")
+    } else {
+        while (result.isValid) {
+            // do something with the result
+            var id = result.value(0)
+            var forename = result.value(1)
+            var surname = result.value(2)
+            var email = result.value(3)
+            var phone = result.value(4)
+            //test.log(id + forename + surname + email + phone)
+            addEntry(forename, surname, email, phone)
+            result.toNext();
+        }
+        test.log("added " + id + " entries in the addressbook application")
+    }
+}
+
+function testSQLiteDB() {
+
+    var conn = SQL.connect({ Driver: "SQLite", Database: "\\PCMDBase.db" });
+
+    var result = conn.query("SELECT id, forename, surname, email, phone FROM addressbook;");
+    if (conn.isValid == false) {
+        alert("Result is not valid!")
+
+    } else {
+        alert("Result is valid!")
+    }
+}
